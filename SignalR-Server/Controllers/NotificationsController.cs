c5894ecd27b611e75371e2b73eaa89ca969a3aad
vladimirkachan <vladimirkachan0@gmail.com>
2021-06-24 11:11:01 +0300

    Lesson2 end

---
+++ SignalR-Server/Controllers/NotificationsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using SignalR_Common;
+using SignalR_Server.Hubs;
+
+namespace SignalR_Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotificationsController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Push([FromBody] Message message, [FromServices] IHubContext<NotificationHub, INotificationClient> hubContext)
+        {
+            await hubContext.Clients.All.Send(message);
+            return Ok();
+        }
+    }
+}
--- SignalR-Server/Hubs/NotificationHub.cs
+++ SignalR-Server/Hubs/NotificationHub.cs
@@ -9,19 +9,35 @@ using SignalR_Common;
 
 namespace SignalR_Server.Hubs
 {
-    public class NotificationHub : Hub
+    public class NotificationHub : Hub<INotificationClient>
     {
         public Task SendMessage(Message message)
         {
             Debug.WriteLine(Context.ConnectionId);
             if (Context.Items.ContainsKey("user_name"))
                 message.Title = $"Message from user: {Context.Items["user_name"]}";
-            return Clients.Others.SendAsync("Send", message);
+            return Clients.Others.Send(message);
         }
         public Task SetName(string name)
         {
             Context.Items.TryAdd("user_name", name);
             return Task.CompletedTask;
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Clients.Others.Send(new Message { Title = $"new client connected {Context.ConnectionId}", Body = "" });
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Clients.Others.Send(new Message { Title = $"new client disconnected {Context.ConnectionId}", Body = "" });
+            return base.OnDisconnectedAsync(exception);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            Debug.WriteLine("Hub disposed");
+            base.Dispose(disposing);
         }
     }
 }
